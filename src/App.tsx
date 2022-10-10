688ab233838d773ccc213dc3631c196fcc645278
Zair15 <zairabbas1533@outlook.com>
2022-10-10 14:36:25 +0500

    mutableRef & DomRef with Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import "./App.css";
-import { UserContextProvider } from "./components/context/UserContext";
-import { User } from "./components/context/User";
+import { MutableRef } from "./components/ref/MutableRef";
 
 function App() {
   return (
     <div className="App">
-      <UserContextProvider>
-        <User />
-      </UserContextProvider>
+      <MutableRef />
     </div>
   );
 }
---
+++ src/components/ref/MutableRef.tsx
@@ -0,0 +1,26 @@
+import { useState, useEffect, useRef } from "react";
+
+export const MutableRef = () => {
+  const [timer, setTimer] = useState(0);
+  const interValRef = useRef<number | undefined>(undefined);
+
+  const stopTimer = () => {
+    window.clearInterval(interValRef.current);
+  };
+
+  useEffect(() => {
+    interValRef.current = window.setInterval(() => {
+      setTimer((timer) => timer + 1);
+    }, 1000);
+    return () => {
+      stopTimer();
+    };
+  }, []);
+
+  return (
+    <div>
+      HookTimer - {timer} -
+      <button onClick={() => stopTimer()}>Stop Timer</button>
+    </div>
+  );
+};
