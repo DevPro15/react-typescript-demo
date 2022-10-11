7fb9b9ebdcd86758934308dccd24f96cdd3c55d5
Zair15 <zairabbas1533@outlook.com>
2022-10-11 12:59:12 +0500

    Polymorphic Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import "./App.css";
+import { Text } from "./components/polymorphic/Text";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Text as="h1" size="lg">
+        Heading
+      </Text>
+      <Text as="p" size="md">
+        Paragraph
+      </Text>
+      <Text as="label" htmlFor="someId" size="sm" color="secondary">
+        Label
+      </Text>
+    </div>
+  );
 }
 
 export default App;
