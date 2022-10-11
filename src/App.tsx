a1a54df2de6133297272311415e5abc4f564ad8b
Zair15 <zairabbas1533@outlook.com>
2022-10-11 11:47:19 +0500

    template literals and exclude

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import { RandomNumber } from "./components/restriction/RandomNumber";
+import { Toast } from "./components/templateliterals/Toast";
 
 function App() {
   return (
     <div className="App">
-      <RandomNumber value={10} isPositive />
+      <Toast position="center" />
     </div>
   );
 }
